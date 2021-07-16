dbb58e4c06706931a9ecb407d69e175f526764f9
Mitch Tyrer <mltcinematography@gmail.com>
2021-07-16 10:22:53 -0600

    converted determining the winner into a function.  updated player input for valid choices only

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,7 +2,12 @@ import random
 
 ### Rock beats Scissors, Scissors beats paper, Paper beats rock
 ##possible update is to verify valid inputs should I so choose.
-options = ["rock", "paper", "scissors"]
+options = ["Rock", "Paper", "Scissors"]
+victory_conditions = {
+        "Rock": "Scissors", #Rock beats Scissors,
+        "Paper": "Rock", #Paper beats Rock
+        "Scissors": "Paper", #scissors beats paper
+    }
 playerScore = 0
 compScore = 0
 
@@ -11,41 +16,34 @@ def computer_play ():
     return selection
 
 def user_play ():
-    selection = input("Rock, Paper, Scissors! ").lower()
-    if selection == 'exit' or selection == 'quit':
-        exit()
-    index = options.index(selection)
-    return index
+    while True:
+        selection = input("Rock, Paper, Scissors! ").lower()
+        opt = [opt.lower() for opt in options]
+        try:
+            selection in opt
+            if selection == 'exit' or selection == 'quit':
+                exit()
+            index = opt.index(selection)
+            return index
+        except:
+            print("Invalid Selection!")
+
+
+def determine_winner (userPlay, computerPlay):
+    defeats = victory_conditions[options[userPlay]]
+    if userPlay != computerPlay:
+        if options[computerPlay] in defeats:
+            print(f"You Win! {options[userPlay]} beats {options[computerPlay]}!")
+        else: print(f"You Lose! {options[computerPlay]} beats {options[userPlay]}!")
+    else:
+        print("It's a Tie!")
 
 def play_round ():
     global playerScore
     global compScore
     computer = computer_play()
     player = user_play()
-    ##Player wins
-    if player != computer:
-        if player == 0 and computer == 2:
-            playerScore += 1
-            print("You win! Rock beats Scissors!")
-        elif player == 1 and computer == 0:
-            playerScore += 1
-            print("You win! Paper beats Rock!")
-        elif player == 2 and computer == 1:
-            playerScore += 1
-            print("You win! Scissors beats paper!")
-    ##Comp wins
-        elif player == 0 and computer == 1:
-            compScore += 1
-            print("You lose! Paper beats Rock!")
-        elif player == 1 and computer == 2:
-            compScore += 1
-            print("You lose! Scissors beats Paper!")
-        elif player == 2 and computer == 0:
-            compScore += 1
-            print("You lose! Rock beats Scissors!")
-    else:
-        print("It's a Tie!")
-
+    determine_winner(player, computer)
 
 
 def game ():
